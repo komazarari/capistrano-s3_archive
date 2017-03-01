f14d91d41a706a4d67ca6e6e41403a74b04f1cd2
Takuto Komazaki <komazarari@gmail.com>
2017-03-01 11:41:55 +0900

    use  test!() instead of test()

--- lib/capistrano/s3_archive.rb
+++ lib/capistrano/s3_archive.rb
@@ -172,7 +172,7 @@ module Capistrano
 
           unless fetch(:rsync_cache).nil?
             cache = rsync_cache
-            link_option = if fetch(:hardlink) && test("[ `readlink #{current_path}` != #{release_path} ]")
+            link_option = if fetch(:hardlink) && test!("[ `readlink #{current_path}` != #{release_path} ]")
                             "--link-dest `readlink #{current_path}`"
                           end
             copy = %(#{fetch(:rsync_copy)} #{link_option} "#{cache}/" "#{release_path}/")
