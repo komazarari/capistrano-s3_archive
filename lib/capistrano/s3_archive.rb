d5b6bdb4c18ca788e2257061a285385b88bae324
Takuto Komazaki <komazarari@gmail.com>
2017-03-10 11:46:47 +0900

    re-fix 05493df

--- lib/capistrano/s3_archive.rb
+++ lib/capistrano/s3_archive.rb
@@ -58,7 +58,7 @@ module Capistrano
           when :master, :latest, nil
             latest_object_key
           else
-            [object_prefix, fetch(:branch)].join('/')
+            object_prefix + fetch(:branch)
           end
       end
 
