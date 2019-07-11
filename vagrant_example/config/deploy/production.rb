5cc3fc4e474ef2c80d7865cb756da1b5c1363daf
Takuto Komazaki <komazarari@gmail.com>
2019-07-11 15:33:01 +0900

    upd

--- vagrant_example/config/deploy/production.rb
+++
@@ -1,7 +0,0 @@
-server 'localhost',
-       user: 'vagrant',
-       roles: ['web', 'app'],
-       ssh_options: {
-         auth_methods: ['publickey'],
-         keys: '/home/vagrant/.ssh/insecure_key'
-       }
